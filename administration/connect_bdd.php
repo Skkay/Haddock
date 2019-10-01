a6ffc22f0e6b5f4154801b8233fa4220471ab92d
Skkay <skkay.thomas@gmail.com>
2019-10-01 19:58:24 +0200

    haddock -> haddock_base
    Changement de nom de la BDD

--- administration/connect_bdd.php
+++ administration/connect_bdd.php
@@ -4,7 +4,7 @@
     error_reporting(0);
 
     try {
-        $bdd = new PDO("mysql:host=localhost;dbname=haddock;charset=utf8", "root", "");
+        $bdd = new PDO("mysql:host=localhost;dbname=haddock_base;charset=utf8", "root", "");
     }
     catch (Exception $e) {
         die("Erreur : " . $e->getMessage());
