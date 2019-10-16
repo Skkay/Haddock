b4197f138b05f5baa890d05003cdd4f83007df1d
Skkay <skkay.thomas@gmail.com>
2019-10-16 20:14:46 +0200

    Possibilité de voir les bonnes et mauvaises réponses à la fin du quiz
    Ajout d'un bouton permettant d'afficher les bonnes réponses en vert, et les mauvaises réponses en rouge avec la correction en fin de quiz.

--- jeux/quiz_resultat.php
+++ jeux/quiz_resultat.php
@@ -31,6 +31,7 @@
     
     <link href="https://fonts.googleapis.com/css?family=Francois+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="quiz_style.css">
+    <script src="https://kit.fontawesome.com/e1c78fd441.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -45,16 +46,50 @@
             <hr class="my-4">
 			<div class="card text-center">
 				<div class="card-body">
-					<div class="quiz_reponse"><?php echo($score_final); ?></div>
+					<div class="quiz_resultat"><?php echo($score_final); ?></div>
 					<?php 
 					if($points < 2) {
-						echo('<div class="quiz_reponse quiz_label">bonne réponse</div>');
+						echo('<div class="quiz_resultat quiz_label">bonne réponse</div>');
 					}
 					else {
-						echo('<div class="quiz_reponse quiz_label">bonnes réponses</div>');
+						echo('<div class="quiz_resultat quiz_label">bonnes réponses</div>');
 					}
 					?>
 					<a href="index.php" class="btn btn-secondary" role="button">Retour aux jeux</a>	
+					<a class="btn btn-secondary" data-toggle="collapse" href="#reponses" role="button" aria-expanded="false" aria-controls="reponses">Voir les réponses</a>			
+				</div>
+			</div>
+			<br>
+			<div class="collapse" id="reponses">
+				<div class="card">
+					<div class="card-body">
+					<?php
+						$bdd_reponse = $bdd->query("
+					        SELECT question, rep_1, (SELECT COUNT(*) FROM quiz) AS nb_question
+					        FROM quiz
+					    ");
+						$num_question = 0;
+						while($row = $bdd_reponse->fetch()) {
+							$num_question++;
+
+							echo("<div>");
+							echo('<p class="quiz_question">'.$num_question.' : '.$row["question"].'</p>');
+
+							// En cas de bonne réponse
+							if($row["rep_1"] == $_POST["reponse".$num_question]) {
+								echo('<p class="quiz_bonne_rep"><i class="fas fa-check"></i> '.$_POST["reponse".$num_question].'</p>');
+							}
+
+							// En cas de mauvaise réponse
+							else {
+								echo('<p class="quiz_mauvaise_rep"><i class="fas fa-times"></i> '.$_POST["reponse".$num_question].'</p>');
+								echo('<p class="quiz_bonne_rep"><i class="fas fa-check"></i> '.$row["rep_1"].'</p>');
+							}
+							echo("<br>");
+							echo("</div>");
+						}
+					?>
+					</div>
 				</div>
 			</div>
         </div>
